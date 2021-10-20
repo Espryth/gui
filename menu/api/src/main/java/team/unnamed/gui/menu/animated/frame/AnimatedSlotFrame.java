8ecc33c48a4b7afb4b26c2eeb89f425a915b813a
pixeldev <amiranda8695@gmail.com>
2021-10-20 15:04:30 -0500

    feature: adding another factory method.

--- menu/api/src/main/java/team/unnamed/gui/menu/animated/frame/AnimatedSlotFrame.java
+++ menu/api/src/main/java/team/unnamed/gui/menu/animated/frame/AnimatedSlotFrame.java
@@ -20,6 +20,10 @@ public class AnimatedSlotFrame {
         return delay;
     }
 
+    public static AnimatedSlotFrame create(ItemClickable item) {
+        return create(item, 20);
+    }
+
     public static AnimatedSlotFrame create(ItemClickable item, int delay) {
         return new AnimatedSlotFrame(item, delay);
     }
